c3f11334f10137f2ec2b2977af35353194e1b678
unknown <kartikgarg938@gmail.com>
2022-04-26 01:44:08 +0530

    adding redis db

--- api/database/database.go
+++ api/database/database.go
@@ -10,12 +10,12 @@ import(
 var Ctx = context.Background()
 
 func createClient(dbNo int) *redis.Client{
-	rdb := redis.NewClient(&redis.Options){
+	rdb := redis.NewClient(&redis.Options{
 		//to create a new db client we need to pass 3 things to redis
 		Addr:		os.Getenv("DB_ADDR"),
 		Password:	os.Getenv("DB_PASS"),
 		DB:			dbNo,
-	}
+	})
 
 	return rdb
 }
