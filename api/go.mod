b99fbd82235147833be1852b93ac1bfe35593c77
unknown <kartikgarg938@gmail.com>
2022-04-25 21:00:22 +0530

    added env variables and setup the main.go file

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,18 @@
 module url-shortener
 
 go 1.17
+
+require github.com/gofiber/fiber/v2 v2.32.0
+
+require github.com/Valgard/go-pcre v0.0.0-20200510215507-235e400e25e9 // indirect
+
+require (
+	github.com/Valgard/godotenv v0.0.0-20200511222744-8873b92a09c5
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.35.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"url-shortener/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+//putting all routes in the below function
+func setupRoutes(app *fiber.App){
+	app.Get(":/url", routes.ResolveUrl)
+	app.Post("/api/v1", routes.ShortenUrl)
+}
+
+func main(){
+	err := godotenv.Load()
+
+	if(err!=nil){
+		fmt.Println(err)
+	}
+	//app above works same as how it works in express
+	app := fiber.New()
+
+	//adding logger to keep logs in place
+	app.Use(logger.New())
+
+	//calling routes function
+	setupRoutes(app)
+
+	//starting the server, and taking port from the .env file 
+	//fatal is like print in log followed by os.Exit(1), basically exiting 
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
