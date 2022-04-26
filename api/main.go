7d3e3b61eacfa24eebcc0dfb2ba11541eca33bd6
unknown <kartikgarg938@gmail.com>
2022-04-27 01:39:55 +0530

    adding small last changes

--- api/main.go
+++ api/main.go
@@ -3,35 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"url-shortener/routes"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
 //putting all routes in the below function
-func setupRoutes(app *fiber.App){
+func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveUrl)
 	app.Post("/api/v1", routes.ShortenUrl)
+
 }
 
-func main(){
+func main() {
 	err := godotenv.Load()
 
-	if(err!=nil){
+	if err != nil {
 		fmt.Println(err)
 	}
 	//app above works same as how it works in express
 	app := fiber.New()
 
 	//adding logger to keep logs in place
-	app.Use(logger.New())
+	//app.Use(logger.New())
 
 	//calling routes function
 	setupRoutes(app)
 
-	//starting the server, and taking port from the .env file 
-	//fatal is like print in log followed by os.Exit(1), basically exiting 
-	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+	//starting the server, and taking port from the .env file
+	//fatal is like print in log followed by os.Exit(1), basically exiting
+	log.Fatal(app.Listen(":3000"))
 }
